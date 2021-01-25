f4636698020c600cfeba6f1dea2b8e4af94f99aa
toropovm <toropovm@gmail.com>
2021-01-25 08:57:27 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import * as serviceWorker from './serviceWorker.js';
+import './assets/css/bootstrap.min.css'
+import './assets/css/agency.min.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
+//serviceWorker.unregister();

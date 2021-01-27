47c0dbd6975f58fcc8cbbc174d06a5ba6c76e91c
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-27 17:55:22 -0500

    testing deployment.

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
 
 const httpLink = createHttpLink({
-  uri: 'https://git.heroku.com/enviromania.git'
+  uri: 'https://enviromania.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
